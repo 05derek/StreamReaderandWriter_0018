c554b456f9a1eea55b9e7758894906c8eafc4a52
05derek <derek.dzakir.ft24@mail.umy.ac.id>
2025-06-09 10:02:42 +0700

    memasukan data ke file

--- ioFile.cpp
+++ ioFile.cpp
@@ -1,17 +1,13 @@
-#include <fstream>
-#include <iostream>
-#include <string>
-using namespace std;
 
-int main() {
-    string baris;
 
-    //oprasi file dalam mode menulis
-    ofstream outfile;
-    //menunjuk ke sebuah nama file untuk membuka
-    outfile.open ("contohfile.txt");
-
-    cout << ">=Menulis fie, \'q\' untuk keluar" << endl;
-
-   
+    //unlimited loop untuk menulis
+    while (true) {
+        cout << "- ";
+        //mendapatkan setiap karakter dalam satu baris
+        getline(cin, baris);
+        //loop akan berhenti jika anda memasukkan karakter q
+        if (baris == "q" ) break;
+        //menulis dan memasukkan nilai dari 'baris' ke dalam file
+        outfile << baris << endl;
+    }
 }
