59f8ab47d6649d1495438b88f291db0059026b83
05derek <derek.dzakir.ft24@mail.umy.ac.id>
2025-06-09 09:59:53 +0700

    membuat file

---
+++ ioFile.cpp
@@ -0,0 +1,17 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string baris;
+
+    //oprasi file dalam mode menulis
+    ofstream outfile;
+    //menunjuk ke sebuah nama file untuk membuka
+    outfile.open ("contohfile.txt");
+
+    cout << ">=Menulis fie, \'q\' untuk keluar" << endl;
+
+   
+}
