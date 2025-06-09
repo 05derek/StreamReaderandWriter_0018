388218d87a98e9fd056f4e10e0d4e0f9c423bd20
05derek <derek.dzakir.ft24@mail.umy.ac.id>
2025-06-09 10:45:04 +0700

    membuat pesan array 3 elemen dan mencoba penangkapan obyek ke 5

--- manualExeption.cpp
+++ manualExeption.cpp
@@ -1,4 +1,17 @@
+#include <iostream>
+using namespace std;
 
+int main()
+{
+    try{
+        cout << "Selamat Belajar di Prodi TI UMY" << endl;
+        throw 0.5;//melemparkan sebuah integer maka
+        cout << "Pernyataan tidak akan diekskusi" << endl;
+    }
+    catch (int a) {
+        //blok ini akan di ekskusi
+        cout << "Pengecualian akan diekskusi" << endl;
+    }
     catch (...) {
         /*jika selain integer maka block ini akan diekskusi*/
         cout << "default Pengecualian diekskusi" << endl;
