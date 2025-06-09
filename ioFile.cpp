d6121b01b462f7bc5c031218b64b561b6cd99a65
05derek <derek.dzakir.ft24@mail.umy.ac.id>
2025-06-09 10:11:28 +0700

    perintah jika file yang kita buat ada

--- ioFile.cpp
+++ ioFile.cpp
@@ -1,13 +1,14 @@
 
-    //selesai dalam menulis sekarang tutup filenya
-    outfile.close();
-
-    //oprasi file dalam mode membaca
-    ifstream infile;
-    //enunjuk ke sebuah file untuk membuka
-    infile.open("contohfile.txt");
-
-    cout << endl << ">= Membuka dan membaca file " << endl;
     //jika file ada maka
     if (infile.is_open())
+    {
+        //melakukan perulangan setiap baris
+        while (getline(infile, baris))
+        {
+            //dan tampilkan disini
+            cout << baris << '\n';
+        }
+        //tutup file tersebut setelah selesai
+        infile.close();
+    }
 }
